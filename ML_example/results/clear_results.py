8556c2c2d863a5747d4f62ec9c92f9148e47bfaf
je-griffiths <150913254+je-griffiths@users.noreply.github.com>
2024-12-06 11:36:55 +0000

    Update clear_results.py

--- ML_example/results/clear_results.py
+++ ML_example/results/clear_results.py
@@ -2,13 +2,21 @@ import os
 
 for f in os.listdir():
     if f not in ["clear_results.py", "plot_pareto.py"]:
-        os.remove(f)
+        if os.path.isfile(f):
+            os.remove(f)
+        elif os.path.isdir(f):
+            continue
 
 os.chdir("../classification/classification_results/")
 for f in os.listdir():
-    os.remove(f)
+    if os.path.isfile(f):
+        os.remove(f)
+    elif os.path.isdir(f):
+        continue
 
 os.chdir("../../similarity_ranking/ranking_results/")
 for f in os.listdir():
-    os.remove(f)
-
+    if os.path.isfile(f):
+        os.remove(f)
+    elif os.path.isdir(f):
+        continue
